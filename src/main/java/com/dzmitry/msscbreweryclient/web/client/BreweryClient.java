c472c0df07b5be7e61408c5e15aa2109a3cc503f
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-10-25 16:22:51 +0200

    crud methods for CustomerDto with tests

--- src/main/java/com/dzmitry/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/com/dzmitry/msscbreweryclient/web/client/BreweryClient.java
@@ -1,6 +1,7 @@
 package com.dzmitry.msscbreweryclient.web.client;
 
 import com.dzmitry.msscbreweryclient.web.model.BeerDto;
+import com.dzmitry.msscbreweryclient.web.model.CustomerDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ import java.util.UUID;
 public class BreweryClient {
 
     public final String BEER_PATH_V1 = "/api/v1/beer/";
+    public final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
     private String apihost;
     private final RestTemplate restTemplate;
 
@@ -39,5 +41,21 @@ public class BreweryClient {
 
     public void setApihost(String apihost) {
         this.apihost = apihost;
+    }
+
+    public CustomerDto getCustomerById(UUID randomUUID) {
+        return restTemplate.getForObject(apihost + CUSTOMER_PATH_V1 + randomUUID.toString(), CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto) {
+        return restTemplate.postForLocation(apihost + CUSTOMER_PATH_V1, customerDto);
+    }
+
+    public void updateCustomer(UUID randomUUID, CustomerDto customerDto) {
+        restTemplate.put(apihost + CUSTOMER_PATH_V1 + randomUUID.toString(), customerDto);
+    }
+
+    public void deleteCustomer(UUID randomUUID) {
+        restTemplate.delete(apihost + CUSTOMER_PATH_V1 + randomUUID.toString());
     }
 }
